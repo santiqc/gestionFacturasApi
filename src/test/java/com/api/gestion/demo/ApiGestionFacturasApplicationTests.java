4dae1b5add6aa46ac5f7bdde4fc43f59a4dd41f7
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2024-02-14 17:56:25 -0500

    Creacion del proyecto

---
+++ src/test/java/com/api/gestion/demo/ApiGestionFacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gestion.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionFacturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
