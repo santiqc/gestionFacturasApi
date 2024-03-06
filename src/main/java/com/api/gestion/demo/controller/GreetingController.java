608c74e3ffe359c734ed60ebe592c8d59dce63e1
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2024-03-06 11:52:39 -0500

    conexion a bd y controller de funcionamiento

---
+++ src/main/java/com/api/gestion/demo/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package com.api.gestion.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/greeting")
+public class GreetingController {
+
+    @GetMapping("/sayHello")
+    public String SayHello(){
+        return "Hello from Api Santi";
+    }
+}
