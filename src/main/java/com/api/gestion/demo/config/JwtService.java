f73e9e94a16e094e11245ec50d6246bacedb0444
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2024-03-07 20:07:03 -0500

    metodos config security y jwt

--- src/main/java/com/api/gestion/demo/config/JwtService.java
+++ src/main/java/com/api/gestion/demo/config/JwtService.java
@@ -2,6 +2,7 @@ package com.api.gestion.demo.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,12 +10,29 @@ import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 
 @Service
 public class JwtService {
 
     private static final String SECRET_KEY = "ZU7VyGAXIsf6vb6vx4hHiVfdqbJRY3CINBn7c8Nai8vqXcrdg3Yd6rrs1rlEy43r";
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Objects> extraClaims, UserDetails userDetails) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
 
     public String getUserName(String jwt) {
         return getClaim(jwt, Claims::getSubject);
